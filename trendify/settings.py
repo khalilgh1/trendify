a46849400572fe040638f45d4dd66d5aed2e251d
KhalilGH <khalil.ghanem@ensia.edu.dz>
2025-06-29 21:36:32 +0100

    fixing deploy issues #5

--- trendify/settings.py
+++ trendify/settings.py
@@ -153,9 +153,7 @@ STATICFILES_DIRS = [
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Media files (uploads)
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# Media files (user-uploaded content)
 
 INSTALLED_APPS += [
     'cloudinary',
