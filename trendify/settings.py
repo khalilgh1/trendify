baa0bc13bf4f2109af1d28d22c1416ff77e6058e
KhalilGH <khalil.ghanem@ensia.edu.dz>
2026-03-20 21:49:40 +0100

    revert back to non temp db

--- trendify/settings.py
+++ trendify/settings.py
@@ -94,7 +94,7 @@ TEMPLATES = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        'NAME': '/tmp/db.sqlite3',
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
